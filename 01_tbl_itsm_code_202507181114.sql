a2e2826a5932fdaea894a0fc28300e0a11b3421f
devops_infra <devops_infra@example.com>
2025-07-18 11:17:50 +0900

    data

---
+++ 01_tbl_itsm_code_202507181114.sql
@@ -0,0 +1,5 @@
+INSERT INTO itsm.itsm_code (id,division,division_name,part,part_name,code,code_name,code_value,level_code,fst_reg_user_id,fst_reg_dttm,last_reg_user_id,last_mod_dttm,flag) VALUES
+	 (3,'CODE','APP 사용코드','JSA_GRADE','JSA_GRADE','3','3','3','-','INS','2025-06-25 12:55:06.130749+09','INS','2025-06-26 14:37:05.111481+09','Y'),
+	 (2,'CODE','APP 사용코드','JSA_GRADE','JSA_GRADE','2','2','2','-','INS','2025-06-25 12:54:58.766374+09','INS','2025-06-26 14:37:08.832306+09','Y'),
+	 (1,'CODE','APP 사용코드','JSA_GRADE','JSA_GRADE','1','1','1','-','INS','2025-06-25 11:26:39.452512+09','INS','2025-06-26 14:37:12.648512+09','Y'),
+	 (4,'CODE','APP 사용코드','JSA_GRADE','JSA_GRADE','44','44','44','-','INS','2025-06-25 12:55:46.109817+09','INS','2025-07-05 12:00:51.064352+09','Y');
